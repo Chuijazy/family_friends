cf14bfeaa4d8550050eadb1c2ced18cc2296382e
Chuijazy <chuijazy.2004@gmail.com>
2025-06-03 17:59:28 +0600

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
-import 'package:family_friends/screens/home_screen.dart';
+import 'package:family_friends/autorization/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const FamilyFriends());
 }
 
@@ -10,6 +13,6 @@ class FamilyFriends extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Family & Friends', home: HomeScreen());
+    return MaterialApp(title: 'Авторизация', home: SplashScreen());
   }
 }
