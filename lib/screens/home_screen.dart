4ef42f19b91395b58f247304c40d2ed51239688d
Chuijazy <chuijazy.2004@gmail.com>
2025-06-03 14:51:47 +0600

    start project

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,82 @@
+import 'package:family_friends/screens/second_screen.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
+
+  final List<Map<String, String>> characters = [
+    {'images': 'assets/characters/grand_mother.jpg', 'label': 'Бабушка'},
+    {'images': 'assets/characters/grand_father.jpg', 'label': 'Дедушка'},
+    {'images': 'assets/characters/mother.jpg', 'label': 'Мама'},
+    {'images': 'assets/characters/father.jpg', 'label': 'Папа'},
+    {'images': 'assets/characters/daughter.jpg', 'label': 'Дочь'},
+    {'images': 'assets/characters/son.jpg', 'label': 'Сын'},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Family & Friends')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 40),
+            const Text(
+              'Выберите персонажа',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: GridView.builder(
+                itemCount: characters.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                ),
+                itemBuilder:
+                    (context, index) => GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => SecondScreen(),
+                          ),
+                        );
+                      },
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.only(top: 10),
+                            height: 150,
+                            width: 150,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white,
+                              image: DecorationImage(
+                                image: AssetImage(characters[index]['images']!),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            characters[index]['label']!,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
